75dd2ac8584d0f8dcafe0d0f2ef5d63229449844
Jordan Riser <riser.jordan@gmail.com>
2017-06-30 11:51:26 -0400

    initial upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  scanData: any = {};
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  startScanner(){
+    this.barcodeScanner.scan().then((barcodeData) => {
+      this.scanData = barcodeData;
+    }, (err) => {
+        // An error occurred
+    });
   }
 
 }
