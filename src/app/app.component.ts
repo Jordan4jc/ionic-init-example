17f33a7f155d9cf0535ee05f6b1c022c9a543c10
Jordan Riser <riser.jordan@gmail.com>
2017-07-11 09:10:13 -0400

    create new jwthelper property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
   rootPage:any;
+  jwtHelper: JwtHelper = new JwtHelper();
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
